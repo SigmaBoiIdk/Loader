43fb76b50cdc4c21004ae70dc711b92c0d5fefb0
RobloxSkids <dyinq7186@gmail.com>
2024-09-15 18:16:26 -0400

    Update WeGonBeOk.lua

--- WeGonBeOk.lua
+++ WeGonBeOk.lua
@@ -1,2 +1,2 @@
-script_key= getgenv().FlowingAway["Skibidi Luarmor FatBoy"].Script_Key
+script_key = getgenv().FlowingAway["Skibidi Luarmor FatBoy"].Script_Key
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0f83fd0d99ac7c414a02e7298a0b31ca.lua"))()
